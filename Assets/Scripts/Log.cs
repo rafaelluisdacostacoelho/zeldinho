cf37547b94192a02f50031861dfa35f928a0a693
Rafael Luis da Costa Coelho <rafaelluisdacostacoelho@gmail.com>
2022-06-19 15:00:02 -0300

    Add more features to enemy, now him turn to sleep when player is out of range.

--- Assets/Scripts/Log.cs
+++ Assets/Scripts/Log.cs
@@ -27,11 +27,49 @@ public class Log : Enemy
         if (Vector3.Distance(target.position, transform.position) <= chaseRadius &&
             Vector3.Distance(target.position, transform.position) > attackRadius)
         {
-            if (currentState == EnemyState.Idle || currentState == EnemyState.Walk && currentState != EnemyState.Idle)
+            if (currentState == EnemyState.Idle || currentState == EnemyState.Walk && currentState != EnemyState.Stagger)
             {
                 Vector3 temp = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+                ChangeAnim(temp - transform.position);
                 myRigidbody.MovePosition(temp);
                 ChangeState(EnemyState.Walk);
+                anim.SetBool("WakeUp", true);
+            }
+        }
+        else if (Vector3.Distance(target.position, transform.position) > chaseRadius)
+        {
+            anim.SetBool("WakeUp", false);
+        }
+    }
+
+    private void SetAnimFloat(Vector2 setVector)
+    {
+        anim.SetFloat("MoveX", setVector.x);
+        anim.SetFloat("MoveY", setVector.y);
+    }
+
+    private void ChangeAnim(Vector2 direction)
+    {
+        if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
+        {
+            if (direction.x > 0)
+            {
+                SetAnimFloat(Vector2.right);
+            }
+            else if (direction.x < 0)
+            {
+                SetAnimFloat(Vector2.left);
+            }
+        }
+        else if (Mathf.Abs(direction.x) < Mathf.Abs(direction.y))
+        {
+            if (direction.y > 0)
+            {
+                SetAnimFloat(Vector2.up);
+            }
+            else if (direction.y < 0)
+            {
+                SetAnimFloat(Vector2.down);
             }
         }
     }
