cdacdde7992919b9e690b0ad7592f46f7f9c4f2f
Rafael Luis da Costa Coelho <rafaelluisdacostacoelho@gmail.com>
2022-06-26 00:34:10 -0300

    Add new fade transition features.

--- Assets/Scripts/ScriptableObjects/VectorValue.cs
+++ Assets/Scripts/ScriptableObjects/VectorValue.cs
@@ -1,7 +1,12 @@
 using UnityEngine;
 
 [CreateAssetMenu]
-public class VectorValue : ScriptableObject
+public class VectorValue : ScriptableObject, ISerializationCallbackReceiver
 {
     public Vector2 initialValue;
+    public Vector2 defaultValue;
+    public void OnAfterDeserialize() {
+        initialValue = defaultValue;
+    }
+    public void OnBeforeSerialize() { }
 }
